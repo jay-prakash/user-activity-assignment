69a695d8e9aa8a393cd89bb8d9d68ce81a3645d1
Jayprakash <jayprakashmandal9@gmail.com>
2020-08-09 19:40:32 +0530

    added whitenoise middleware

--- fullthrottle_assignment/base_settings.py
+++ fullthrottle_assignment/base_settings.py
@@ -25,8 +25,8 @@ INSTALLED_APPS = [
 AUTH_USER_MODEL = 'accounts.User'
 
 MIDDLEWARE = [
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
